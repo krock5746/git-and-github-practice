80676d96b3f41f25f2598020f8a0a6529d345c91
Kyung Kim <Kyung@Kyungs-MacBook-Air.local>
2016-06-07 13:01:18 -0700

    Java file

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+	// write your code here
+        int one = 1;
+        int two = 2;
+        int three = 3;
+    }
+
+}
